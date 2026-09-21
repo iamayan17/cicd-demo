c24f5e8e06be26398edcc313bd60365a3e0d608f
Ayan_17 <123564773+iamayan17@users.noreply.github.com>
2026-09-21 11:44:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello from ci cd ")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Docker!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
